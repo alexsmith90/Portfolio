3125e778a262fad9522326da1935aa2722f4cc2f
alexsmith90 <39973215+alexsmith90@users.noreply.github.com>
2021-02-08 15:29:27 -0500

    Cards and JQuery
    I added an animation action to the humburger menu. I corrected come broken elements on mobile. I also added cards for my skills on the home page.

--- Script.js
+++ Script.js
@@ -1,4 +1,20 @@
 
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+var modal = document.getElementById('ticketModal');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 $(".searchButton").on("click", function(){
 
   $(".searchButton").toggleClass("active");
@@ -15,3 +31,9 @@ $(".searchButton").on("click", function(){
     $(".searchIcon").removeClass("fa-times").addClass("fa-search");
   }
 });
+
+$(".toggle").click(function(){
+  $(this).find(".toggleContainer").toggleClass("active");
+  $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+});
+ 
